08b232a0398b3e56ce9863cd0c77dbcc013d759b
hjonghyeok <whdgur1999@gmail.com>
2023-05-15 17:12:15 +0900

    번역기능 추가

--- main/views.py
+++ main/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import openai
+import os, json
+from pathlib import Path
+from googletrans import Translator
 
-# Create your views here.
+def translate_text(text, target_language):
+    translator = Translator()
+    result = translator.translate(text, dest=target_language)
+    translated_text = result.text
+    return translated_text
+
+# 번역할 텍스트
+
+
+def index(request):
+    text_to_translate = "번역기능 성공"
+    # 번역 실행
+    translated_text = translate_text(text_to_translate, 'en')
+    return HttpResponse(translated_text)
