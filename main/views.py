6b3e90bd95385561bbe4f3ca3b2432c3bb37f44f
hjonghyeok <whdgur1999@gmail.com>
2023-05-16 10:54:18 +0900

    load html,css

--- main/views.py
+++ main/views.py
@@ -5,17 +5,17 @@ import os, json
 from pathlib import Path
 from googletrans import Translator
 
-def translate_text(text, target_language):
-    translator = Translator()
-    result = translator.translate(text, dest=target_language)
-    translated_text = result.text
-    return translated_text
+# def translate_text(text, target_language):
+#     translator = Translator()
+#     result = translator.translate(text, dest=target_language)
+#     translated_text = result.text
+#     return translated_text
 
 # 번역할 텍스트
 
 
 def index(request):
-    text_to_translate = "번역기능 성공"
-    # 번역 실행
-    translated_text = translate_text(text_to_translate, 'en')
-    return HttpResponse(translated_text)
+    # text_to_translate = "번역기능 성공"
+    # # 번역 실행
+    # translated_text = translate_text(text_to_translate, 'en')
+    return render(request, "index.html")
